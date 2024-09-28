ad72babc7a9526de965b0974063c5931b0354ba2
Shizukise <j.chavarria121121@hotmail.com>
2024-09-28 16:38:30 +0200

    project changed

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -6,15 +6,21 @@ import './Login.css'
 
 
 const MainLoginDiv = () => {
+
+
+
+
     return (
         <>
             <Navbar />
             <div id="MainLoginContainer">
                 <div id="MainLoginDiv">
-                    <label htmlFor="inputUsername5" className="form-label">User</label>
-                    <input className="form-control" type="text" placeholder=">" aria-label="default input example" id="inputUsername5" />
-                    <label htmlFor="inputPassword5" className="form-label">Password</label>
-                    <input type="password" id="inputPassword5" className="form-control" placeholder=">" />
+                    <form>
+                        <label htmlFor="inputUsername5" className="form-label">User</label>
+                        <input className="form-control" type="text" placeholder=">" aria-label="default input example" id="inputUsername5" />
+                        <label htmlFor="inputPassword5" className="form-label">Password</label>
+                        <input type="password" id="inputPassword5" className="form-control" placeholder=">" />
+                    </form>
                 </div>
             </div>
         </>
